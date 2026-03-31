2fd1479736b39ff5b76a51bd0c6b00db1dfdab6f
Zael41 <zaelgaming@gmail.com>
2026-03-31 12:41:14 +0200

    simplified pageturn function

--- Assets/Scripts/LegendPanel.cs
+++ Assets/Scripts/LegendPanel.cs
@@ -12,23 +12,11 @@ public class LegendPanel : MonoBehaviour
     private int currentPage;
     #endregion
 
-    public void TurnPageRight()
+    public void TurnPage(bool left)
     {
-        currentPage++;
+        currentPage += left ? -1 : 1;
         if (currentPage > pages.Count - 1) currentPage = 0;
-        for (int i = 0; i < pages.Count; i++)
-        {
-            if (i == currentPage) pages[i].SetActive(true);
-            else pages[i].SetActive(false);
-        }
-        numberText.text = (currentPage + 1).ToString() + "/" + pages.Count;
-        AudioManager.instance.PlaySound("pageTurn", 0.5f);
-    }
-
-    public void TurnPageLeft()
-    {
-        currentPage--;
-        if (currentPage < 0) currentPage = pages.Count - 1;
+        else if (currentPage < 0) currentPage = pages.Count - 1;
         for (int i = 0; i < pages.Count; i++)
         {
             if (i == currentPage) pages[i].SetActive(true);
